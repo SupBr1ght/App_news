f41b86704133ee77ea7b2a95a474bc46f84fbaa6
sap <pechkursasha@gmail.com>
2025-09-16 23:44:16 +0300

    feat: add NewsPage with Chakra styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router";
 import LoginPage from "./components_ui/LoginPage";
 import RegisterPage from "./components_ui/RegisterPage";
 import NewsLinks from "./components_ui/NewsFeed";
+import NewsPage from "./components_ui/OneNews";
 
 export default function App() {
 	return (
@@ -9,6 +10,7 @@ export default function App() {
 			<Route path="/login" element={<LoginPage />} />
 			<Route path="/register" element={<RegisterPage />} />
 			<Route path="/news" element={<NewsLinks />} />
+			<Route path="/news/:id" element={<NewsPage />}></Route>
 			<Route path="*" element={<Navigate to="/login" />} />
 		</Routes>
 	);
---
+++ src/components_ui/OneNews.tsx
@@ -0,0 +1,32 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import { newsData } from "../data/NewsData";
+
+export default function NewsPage() {
+	const { id } = useParams();
+
+	if (!id) return <Text>Wrong</Text>;
+
+	const article = newsData.find((n) => n.id === parseInt(id, 10));
+	console.log("id from useParams:", id);
+
+	if (!article) return <Text>The news didn't found</Text>;
+
+	return (
+		<Box
+			minH="100vh"
+			bg="green.200"
+			color="blackAlpha.800"
+			p={6}
+			display="flex"
+			flexDirection="column"
+			alignItems="center"
+			justifyContent="center"
+		>
+			<Heading mb={4}>{article.title}</Heading>
+			<Text fontSize="lg" textAlign="center">
+				{article.content}
+			</Text>
+		</Box>
+	);
+}
