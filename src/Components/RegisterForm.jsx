729ebb488335011954c72fc0ce258d76f54297e8
sap <pechkursasha@gmail.com>
2025-09-16 15:00:18 +0300

    feat: create login and register forms

---
+++ src/Components/RegisterForm.jsx
@@ -0,0 +1,20 @@
+import { Box,  Button, Input, Stack } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+
+export default function RegisterForm({ onRegister }) {
+  const { register, handleSubmit } = useForm();
+
+  const submit = (data) => {
+    onRegister(data);
+  };
+
+  return (
+    <Box as="form" onSubmit={handleSubmit(submit)} p={4} borderWidth="1px" borderRadius="md">
+      <Stack spacing={3}>
+        <Input placeholder="Email" {...register("email", { required: true })} />
+        <Input type="password" placeholder="Password" {...register("password", { required: true })} />
+        <Button type="submit">Register</Button>
+      </Stack>
+    </Box>
+  );
+}
