8371e9000bc8731ca94fb6239726890f98e01e49
sap <pechkursasha@gmail.com>
2025-09-18 13:01:27 +0300

    feat: add lazy loading to news pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { Flex, Box, Center, Spinner } from "@chakra-ui/react";
 import { Navigate, Route, Routes } from "react-router";
-import NewsLinks from "./modules/pages/NewsFeed";
-import NewsPage from "./modules/pages/OneNews";
 import Header from "./modules/pages/Header";
 import Footer from "./modules/pages/Footer";
 import { lazy, Suspense } from "react";
@@ -11,49 +9,79 @@ const LoginPage = lazy(() => import("./modules/auth/pagesAuth/LoginPage"));
 const RegisterPage = lazy(
 	() => import("./modules/auth/pagesAuth/RegisterPage"),
 );
+const NewsLinks = lazy(() => import("./modules/pages/NewsFeed"));
+const NewsPage = lazy(() => import("./modules/pages/OneNews"));
 
 
 export default function App() {
 	return (
 		<Flex direction="column" minH="100vh">
 			<Header />
-			<Box flex="1" display="flex" flexDirection="column"  overflow="hidden">
+			<Box flex="1" display="flex" flexDirection="column" overflow="hidden">
 				<Routes>
 					<Route element={<Layout />}>
-				<Route
-					path="/login"
-					element={
-						<Suspense
-							fallback={
-								<Center>
-									<Spinner size="xl" color="green.500" />
-								</Center>
+						<Route
+							path="/login"
+							element={
+								<Suspense
+									fallback={
+										<Center>
+											<Spinner size="xl" color="green.500" />
+										</Center>
+									}
+								>
+									<LoginPage />
+								</Suspense>
 							}
-						>
-							<LoginPage />
-						</Suspense>
-					}
-				/>
-				<Route
-					path="/register"
-					element={
-						<Suspense
-							fallback={
-								<Center>
-									<Spinner size="xl" color="green.500" />
-								</Center>
+						/>
+						<Route
+							path="/register"
+							element={
+								<Suspense
+									fallback={
+										<Center>
+											<Spinner size="xl" color="green.500" />
+										</Center>
+									}
+								>
+									<RegisterPage />
+								</Suspense>
 							}
-						>
-							<RegisterPage />
-						</Suspense>
-					}
-				/>
-				<Route path="*" element={<Navigate to="/login" />} />
-			</Route>
-					<Route path="/news" element={<NewsLinks />} />
-					<Route path="/news/:id" element={<NewsPage />} />
+						/>
+						<Route path="*" element={<Navigate to="/login" />} />
+					</Route>
+
+					<Route
+						path="/news"
+						element={
+							<Suspense
+								fallback={
+									<Center>
+										<Spinner size="xl" color="green.500" />
+									</Center>
+								}
+							>
+								<NewsLinks />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="/news/:id"
+						element={
+							<Suspense
+								fallback={
+									<Center>
+										<Spinner size="xl" color="green.500" />
+									</Center>
+								}
+							>
+								<NewsPage />
+							</Suspense>
+						}
+					/>
 					<Route path="*" element={<Navigate to="/login" />} />
 				</Routes>
+
 			</Box>
 
 			<Footer />
