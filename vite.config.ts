beafe8425ac16246f5ebfc3fcdce4e5e4156f60e
sap <pechkursasha@gmail.com>
2025-09-18 14:59:22 +0300

    feat: set neccessary plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,39 @@
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import checker from "vite-plugin-checker";
+import compression from "vite-plugin-compression";
+import Inspect from "vite-plugin-inspect";
+import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss(), tsconfigPaths()],
+	plugins: [
+		react(),
+		svgr(),
+		checker({
+			typescript: true,
+			eslint: {
+				lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+				dev: {
+					logLevel: ["error", "warning"],
+				},
+			},
+		}),
+		compression({ algorithm: "brotliCompress" }),
+		Inspect(),
+		visualizer({ open: true, gzipSize: true }),
+	],
+	build: {
+		minify: "terser",
+		terserOptions: {
+			compress: { drop_console: true },
+		},
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					vendor: ["react", "react-dom", "@chakra-ui/react"],
+				},
+			},
+		},
+	},
 });
